e4e7defc73e04d8e3c4eb454fc6ee39693bf6982
Phusit Somboonyingsuk <phusitsom@gmail.com>
2023-02-09 22:15:32 +0700

    Add POST template for play-record

--- routers/record.py
+++ routers/record.py
@@ -12,11 +12,16 @@ async def get_all_play_records() -> List[Record]:
     pass
 
 
-@router.get("/user/{username}/", response_model=List[Record])
-async def get_user_play_records(username: str) -> List[Record]:
+@router.get("/beatmap/{beatmap_id}/", response_model=List[Record])
+async def get_beatmap_play_records(beatmap_id: int) -> List[Record]:
     pass
 
 
-@router.get("/beatmap/{beatmap_id}/", response_model=List[Record])
-async def get_beatmap_play_records(beatmap_id: int) -> List[Record]:
+@router.post("/beatmap/{beatmap_id}/", response_model=Record)
+async def post_beatmap_play_record(beatmap_id: int, record: Record) -> Record:
+    pass
+
+
+@router.get("/user/{username}/", response_model=List[Record])
+async def get_user_play_records(username: str) -> List[Record]:
     pass
