fba7f7e13d481d12c2ff5b58aa5685e54d626e0d
Phusit Somboonyingsuk <phusitsom@gmail.com>
2023-02-09 21:42:06 +0700

    Add handler for ValueError

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 from fastapi import FastAPI
+from fastapi.responses import JSONResponse
 
-from routers import game_state, song, user
+from routers import beatmap, gamestate, user
 
 app = FastAPI()
 
-app.include_router(game_state.router)
-app.include_router(song.router)
+app.include_router(gamestate.router)
+app.include_router(beatmap.router)
 app.include_router(user.router)
+
+@app.exception_handler(ValueError)
+def handle_value_error(_, exc):
+    return JSONResponse(
+        status_code=400,
+        content={"message": str(exc)},
+    )
