f195a5add4aa01244e2b0f4383886d5b10be2402
Phusit Somboonyingsuk <phusitsom@gmail.com>
2023-02-10 17:08:34 +0700

    Remove beatmap mpdel

--- routers/beatmap.py
+++ routers/beatmap.py
@@ -3,31 +3,21 @@ from typing import List
 from fastapi import APIRouter
 
 from core.database import beatmap
-from models.beatmap import Beatmap, BeatmapInfo
+from models.beatmap import BeatmapInfo
+
 
 router = APIRouter(prefix="/beatmap")
-
 
 @router.get("/info/", response_model=List[BeatmapInfo])
 async def get_all_beatmap_info() -> List[BeatmapInfo]:
     beatmap_info_list = list(beatmap.find({}, {"_id": 0}))
     for beatmap_info in beatmap_info_list:
-        beatmap_info.pop('beats', None)
+        beatmap_info.pop("beats", None)
     return beatmap_info_list
 
 
 @router.get("/info/{beatmap_id}/", response_model=BeatmapInfo)
 async def get_beatmap(beatmap_id: int) -> BeatmapInfo:
     beatmap_info = beatmap.find_one({"beatmap_id": beatmap_id}, {"_id": 0})
-    beatmap_info.pop('beats', None)
+    beatmap_info.pop("beats", None)
     return beatmap_info
-
-
-@router.get("/", response_model=List[Beatmap])
-async def get_all_beatmaps() -> List[Beatmap]:
-    return list(beatmap.find({}, {"_id": 0}))
-
-
-@router.get("/{beatmap_id}/", response_model=Beatmap)
-async def get_beatmap(beatmap_id: int) -> Beatmap:
-    return beatmap.find_one({"beatmap_id": beatmap_id}, {"_id": 0})
