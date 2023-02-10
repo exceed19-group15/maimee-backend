851f0050a1cc0c4694c7421cb87005376db88621
Panupun <panupan.ja@ku.th>
2023-02-10 10:57:16 +0700

    update main call policy

--- main.py
+++ main.py
@@ -1,13 +1,25 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse
 
 from routers import beatmap, gamestate, record
 
+origins = ["*"]
+
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(gamestate.router)
 app.include_router(beatmap.router)
 app.include_router(record.router)
+
 
 @app.exception_handler(ValueError)
 def handle_value_error(_, exc):
