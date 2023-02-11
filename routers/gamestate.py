5100e34cad9b0642b739600bea1a2cd492a4e6ca
Phusit Somboonyingsuk <phusitsom@gmail.com>
2023-02-11 10:43:16 +0700

    Fix game-state returning beatmap_id as null

--- routers/gamestate.py
+++ routers/gamestate.py
@@ -13,16 +13,17 @@ current_beatmap = None
 
 @router.get("/", response_model=GameState)
 async def get_game_state() -> GameState:
-    return GameState(game_state=current_game_state, beatmap=current_beatmap)
+    return GameState(game_state=current_game_state, beatmap_id=current_beatmap)
 
 
 @router.post("/", response_model=Dict[str, str])
 async def set_game_state(game_state: GameState) -> Dict[str, str]:
-    global current_game_state, current_beatmap
 
     if game_state.game_state == PLAYING and game_state.beatmap_id is None:
         raise ValueError("beatmap must be set when game_state is PLAYING")
 
+    global current_game_state, current_beatmap
+    print(f"game_state: {game_state.game_state}, beatmap: {game_state.beatmap_id}")
     current_game_state = game_state.game_state
     current_beatmap = game_state.beatmap_id
 
